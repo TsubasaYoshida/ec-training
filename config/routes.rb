cf82f6a8088f04e28b44529428e08088c502eb3c
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-06-21 14:10:08 +0900

    deviseのサインアップパラメータを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+  }
   resources :products
+
+  root to: 'products#index'
 end
