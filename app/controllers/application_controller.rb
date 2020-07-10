369e2a5f1917c17ab7553cb7d7ce8d62229b7485
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-07-10 23:57:57 +0900

    #3 ログイン状態でのカート取得処理を実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_cart
-    cart = Cart.find_or_create_by(id: session[:cart_id])
-    session[:cart_id] = cart.id
-    cart
+    if current_user
+      Cart.find_or_create_by(user: current_user)
+    else
+      cart = Cart.find_or_create_by(id: session[:cart_id])
+      session[:cart_id] = cart.id
+      cart
+    end
   end
 
   protected
