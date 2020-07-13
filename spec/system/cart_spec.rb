a5142dd84e36e300003c44180fca0b0ba7dae966
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-07-13 23:22:52 +0900

    #3 ログイン状態のカート生成ロジックのSystem Specを書く

--- spec/system/cart_spec.rb
+++ spec/system/cart_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe 'cart', type: :system do
   before do
-    create(:product_1)
-    create(:product_2)
+    @product_1 = create(:product_1)
+    @product_2 = create(:product_2)
   end
 
   scenario '商品をカートに入れる' do
@@ -53,5 +53,34 @@ RSpec.describe 'cart', type: :system do
     }.to change { Cart.count }.by(0)
 
     expect(Cart.count).to eq 1
+  end
+
+  context 'ログイン済みの場合' do
+    before do
+      @user = create(:user)
+      @user.confirm
+      sign_in @user
+    end
+    it 'はじめて商品をカートに入れる' do
+      visit product_path(1)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(1)
+    end
+    it '商品を既存のカートに追加する' do
+      cart = Cart.create!(user: @user)
+      CartDetail.create!(
+        cart: cart,
+        product: @product_1,
+        quantity: 1
+      )
+
+      visit product_path(2)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(0)
+    end
   end
 end
