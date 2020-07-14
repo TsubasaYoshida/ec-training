66b7a3e9336dbc4068c5c159639b6e2667fba9df
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-07-14 23:55:42 +0900

    #3 RSpecをリファクタリングする

--- spec/factories/cart_details.rb
+++ spec/factories/cart_details.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :cart_detail do
     association :cart, factory: :cart
-    association :product, factory: :product
+    association :product, factory: :product_1
     quantity { 1 }
   end
 end
--- spec/system/cart_spec.rb
+++ spec/system/cart_spec.rb
@@ -8,51 +8,46 @@ RSpec.describe 'cart', type: :system do
     @product_2 = create(:product_2)
   end
 
-  scenario '商品をカートに入れる' do
-    visit product_path(1)
-    fill_in '数量', with: 1
+  context '未ログインの場合' do
+    it 'はじめて商品をカートに入れる' do
+      visit product_path(1)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(1)
+      expect(current_path).to eq cart_path
+      expect(page).to have_content '商品1'
+      expect(page).to have_content '数量: 1'
+      expect(page).to have_selector '.cart-subtotal', text: '¥1,000'
+    end
 
-    expect {
-      click_on 'カートに入れる'
-    }.to change { Cart.count }.by(1)
-    expect(current_path).to eq cart_path
-    expect(page).to have_content '商品1'
-    expect(page).to have_content '数量: 1'
-    expect(page).to have_selector '.cart-subtotal', text: '¥1,000'
-  end
+    it '商品をカートに入れた後に、セッション削除して、再度カートに入れる' do
+      visit product_path(1)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(1)
+      reset_session!
+      visit product_path(1)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(1)
+      expect(Cart.count).to eq 2
+    end
 
-  scenario '商品をカートに入れた後に、セッション削除して、再度カートに入れる' do
-    visit product_path(1)
-    fill_in '数量', with: 1
-    expect {
-      click_on 'カートに入れる'
-    }.to change { Cart.count }.by(1)
-
-    reset_session!
-
-    visit product_path(1)
-    fill_in '数量', with: 1
-    expect {
-      click_on 'カートに入れる'
-    }.to change { Cart.count }.by(1)
-
-    expect(Cart.count).to eq 2
-  end
-
-  scenario '商品をカートに入れた後に、違う商品をカートに入れる' do
-    visit product_path(1)
-    fill_in '数量', with: 1
-    expect {
-      click_on 'カートに入れる'
-    }.to change { Cart.count }.by(1)
-
-    visit product_path(2)
-    fill_in '数量', with: 1
-    expect {
-      click_on 'カートに入れる'
-    }.to change { Cart.count }.by(0)
-
-    expect(Cart.count).to eq 1
+    it '商品をカートに入れた後に、違う商品をカートに入れる' do
+      visit product_path(1)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(1)
+      visit product_path(2)
+      fill_in '数量', with: 1
+      expect {
+        click_on 'カートに入れる'
+      }.to change { Cart.count }.by(0)
+    end
   end
 
   context 'ログイン済みの場合' do
@@ -61,6 +56,7 @@ RSpec.describe 'cart', type: :system do
       @user.confirm
       sign_in @user
     end
+
     it 'はじめて商品をカートに入れる' do
       visit product_path(1)
       fill_in '数量', with: 1
@@ -68,6 +64,7 @@ RSpec.describe 'cart', type: :system do
         click_on 'カートに入れる'
       }.to change { Cart.count }.by(1)
     end
+
     it '商品を既存のカートに追加する' do
       cart = Cart.create!(user: @user)
       CartDetail.create!(
@@ -75,7 +72,6 @@ RSpec.describe 'cart', type: :system do
         product: @product_1,
         quantity: 1
       )
-
       visit product_path(2)
       fill_in '数量', with: 1
       expect {
